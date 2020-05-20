97a07be0933c33e344ccece299c7e7d58747f4d0
Cheng <phynk_macheng@outlook.com>
2020-05-20 17:15:17 -0400

    nucleation_rate.py -- parallel programming.

--- nucleation_rate.py
+++ nucleation_rate.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 import os 
-
-
+import multiprocessing as mp
     
 
 def convert_index(index, N):
@@ -70,7 +69,6 @@ def cluster_division(cluster, index_before, index_after, index_add):
             index_rest = np.setdiff1d(index_rest, index_rest[0])
     return cluster
 
-
 def nucleation(dynamics, degree, c, N, sigma, realization, interval, bound=1/2, T_start=0, T_end=100, dt=0.01):
     """TODO: Docstring for nucleation.
 
@@ -81,35 +79,49 @@ def nucleation(dynamics, degree, c, N, sigma, realization, interval, bound=1/2, 
     """
     t = np.arange(T_start, T_end, dt*interval)
     t_num = np.size(t)
+    number_l_set = np.zeros((t_num))
+    cluster_set = np.zeros((t_num))
+
+    des_evo = '../data/' + dynamics + str(degree) + '/size' + str(N) + '/c' + str(c) + '/strength=' + str(sigma) + '/evolution/'
+    evolution_file = des_evo + f'realization{realization}_T_{T_start}_{T_end}.npy'
+    evolution = np.load(evolution_file)
+    evolution_interval = evolution[::interval]
+    x_ave = np.mean(evolution_interval, 1)
+    x_l = x_ave[0]
+    x_h = x_ave[-1]
+    rho = (evolution_interval - x_l)/(x_h-x_l)  # global state
+    index_before = []
+    cluster = []
+    for i in range(t_num):
+        h_index = np.where(rho[i] > bound)[0]
+        number_l_set[i] = N - np.size(h_index)
+        if np.size(h_index)>0 and np.size(h_index)<N:
+            index_after = h_index
+            index_add = np.setdiff1d(index_after, index_before)
+            cluster = cluster_division(cluster, index_before, index_after, index_add)
+            index_before = index_after
+            cluster_set[i] = len(cluster)
+        elif np.size(h_index) == N:
+            break
+    number_nucleation = np.hstack((cluster_set[0], np.diff(cluster_set)))
+    return t, cluster_set, number_l_set, number_nucleation
+
+def nucleation_parallel(arg1):
+    """TODO: Docstring for nucleation_parallel.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    p = mp.Pool(cpu_number)
     realization_num = np.size(realization)
     number_l_set = np.zeros((realization_num, t_num))
     cluster_set = np.zeros((realization_num, t_num))
     nucleation_set = np.zeros((realization_num, t_num))
-    des_evo = '../data/' + dynamics + str(degree) + '/size' + str(N) + '/c' + str(c) + '/strength=' + str(sigma) + '/evolution/'
-    for j in realization:
-        evolution_file = des_evo + f'realization{j}_T_{T_start}_{T_end}.npy'
-        evolution = np.load(evolution_file)
-        evolution_interval = evolution[::interval]
-        x_ave = np.mean(evolution_interval, 1)
-        x_l = x_ave[0]
-        x_h = x_ave[-1]
-        rho = (evolution_interval - x_l)/(x_h-x_l)  # global state
-        index_before = []
-        cluster = []
+    t, cluster_set[i], number_l_set[i], nucleation_set[i] =  p.starmap_async(nucleation, [(dynamics, degree, c, N, sigma, reali, interval) for reali, i in zip(realization, range(realization_num))]).get()
+    p.close()
+    p.join()
 
-        for i in range(t_num):
-            h_index = np.where(rho[i] > bound)[0]
-            number_l_set[j, i] = N - np.size(h_index)
-            if np.size(h_index)>0 and np.size(h_index)<N:
-                index_after = h_index
-                index_add = np.setdiff1d(index_after, index_before)
-                cluster = cluster_division(cluster, index_before, index_after, index_add)
-                index_before = index_after
-                cluster_set[j, i] = len(cluster)
-            elif np.size(h_index) == N:
-                break
-        number_nucleation = np.hstack((cluster_set[j, 0], np.diff(cluster_set[j])))
-    return t, cluster_set, number_l_set, number_nucleation
 
 dynamics = 'mutual'
 degree = 4
@@ -118,5 +130,6 @@ N = 10000
 sigma = 0.1
 realization = [0]
 interval = 100
+cpu_number = 10
 
 t, cluster_set, number_l_set, number_nucleation = nucleation(dynamics, degree, c, N, sigma, realization, interval)
